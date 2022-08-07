9f41d2f3b4776dcb120146e9c7b238fcb211e36a
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2022-08-07 01:45:21 -0700

    clean up

--- src/main/java/xaeroplus/mixin/client/MixinGuiMap.java
+++ src/main/java/xaeroplus/mixin/client/MixinGuiMap.java
@@ -33,53 +33,8 @@ public abstract class MixinGuiMap extends ScreenBase implements IRightClickableE
 
     @Inject(method = "changeZoom", at = @At(value = "HEAD"), cancellable = true)
     private void changeZoom(double factor, int zoomMethod, CallbackInfo ci) {
+        // todo: restore ctrl zoom
         destScale *= Math.pow(1.2D, factor);
         ci.cancel();
     }
-//    @Redirect(method = "changeZoom", at = @At(value = "HEAD"))
-//    private void changeZoom(double factor, int zoomMethod) {
-//        this.closeDropdowns();
-//        this.lastZoomMethod = zoomMethod;
-//        this.cameraDestinationAnimX = null;
-//        this.cameraDestinationAnimZ = null;
-//        if (isCtrlKeyDown()) {
-//            double destScaleBefore = destScale;
-//            if (destScale >= 1.0D) {
-//                if (factor > 0.0D) {
-//                    destScale = Math.ceil(destScale);
-//                } else {
-//                    destScale = Math.floor(destScale);
-//                }
-//
-//                if (destScaleBefore == destScale) {
-//                    destScale += factor > 0.0D ? 1.0D : -1.0D;
-//                }
-//
-//                if (destScale == 0.0D) {
-//                    destScale = 0.5D;
-//                }
-//            } else {
-//                double reversedScale = 1.0D / destScale;
-//                double log2 = Math.log(reversedScale) / Math.log(2.0D);
-//                if (factor > 0.0D) {
-//                    log2 = Math.floor(log2);
-//                } else {
-//                    log2 = Math.ceil(log2);
-//                }
-//
-//                destScale = 1.0D / Math.pow(2.0D, log2);
-//                if (destScaleBefore == destScale) {
-//                    destScale = 1.0D / Math.pow(2.0D, log2 + (double) (factor > 0.0D ? -1 : 1));
-//                }
-//            }
-//        } else {
-//            destScale *= Math.pow(1.2D, factor);
-//        }
-//
-//        if (destScale < 0.0025D) {
-//            destScale = 0.0025D;
-//        } else if (destScale > 50.0D) {
-//            destScale = 50.0D;
-//        }
-//    }
 }

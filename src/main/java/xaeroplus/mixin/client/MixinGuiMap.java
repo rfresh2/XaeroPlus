828ea0d941da20112db8038ba1f27b17f788559c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2022-08-07 01:46:14 -0700

    remove unused shadows

--- src/main/java/xaeroplus/mixin/client/MixinGuiMap.java
+++ src/main/java/xaeroplus/mixin/client/MixinGuiMap.java
@@ -5,9 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xaero.map.animation.SlowingAnimation;
 import xaero.map.gui.GuiMap;
 import xaero.map.gui.IRightClickableElement;
 import xaero.map.gui.ScreenBase;
@@ -20,16 +18,7 @@ public abstract class MixinGuiMap extends ScreenBase implements IRightClickableE
     }
 
     @Shadow
-    protected abstract void closeDropdowns();
-
-    @Shadow
     private static double destScale;
-    @Shadow
-    private int lastZoomMethod;
-    @Shadow
-    private SlowingAnimation cameraDestinationAnimX = null;
-    @Shadow
-    private SlowingAnimation cameraDestinationAnimZ = null;
 
     @Inject(method = "changeZoom", at = @At(value = "HEAD"), cancellable = true)
     private void changeZoom(double factor, int zoomMethod, CallbackInfo ci) {

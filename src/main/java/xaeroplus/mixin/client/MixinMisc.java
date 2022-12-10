b96c357de3beb08234c3440ef7c07a0d11d30d61
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2022-12-10 11:32:14 -0800

    fix skip background render crash when turned on

--- src/main/java/xaeroplus/mixin/client/MixinMisc.java
+++ src/main/java/xaeroplus/mixin/client/MixinMisc.java
@@ -2,7 +2,9 @@ package xaeroplus.mixin.client;
 
 import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xaero.map.misc.Misc;
 import xaeroplus.XaeroPlusSettingRegistry;
 
@@ -13,8 +15,10 @@ public class MixinMisc {
      * @author rfresh2
      * @reason skipping world render messes up some events on hack clients
      */
-    @Overwrite
-    public static boolean screenShouldSkipWorldRender(GuiScreen screen, boolean checkOtherMod) {
-        return XaeroPlusSettingRegistry.skipWorldRenderSetting.getBooleanSettingValue();
+    @Inject(method = "screenShouldSkipWorldRender", at = @At("HEAD"), cancellable = true)
+    private static void screenShouldSkipWorldRender(GuiScreen screen, boolean checkOtherMod, CallbackInfoReturnable<Boolean> cir) {
+        if (!XaeroPlusSettingRegistry.skipWorldRenderSetting.getBooleanSettingValue()) {
+            cir.setReturnValue(false);
+        }
     }
 }

8a42a76bad228956bd9835fc7229cf392af87cc0
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-02 01:02:58 -0700

    fix race condition crash

--- src/main/java/xaeroplus/mixin/client/MixinWaypointsIngameRenderer.java
+++ src/main/java/xaeroplus/mixin/client/MixinWaypointsIngameRenderer.java
@@ -14,7 +14,13 @@ public class MixinWaypointsIngameRenderer {
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lxaero/common/minimap/waypoints/WaypointsManager;getDimensionDivision(Ljava/lang/String;)D"))
     public double redirectDimensionDivision(final WaypointsManager waypointsManager, final String worldContainerID) {
-        final int dim = Minecraft.getMinecraft().world.provider.getDimension();
+        int dim;
+        try {
+            dim = Minecraft.getMinecraft().world.provider.getDimension();
+        } catch (Exception e) {
+            return waypointsManager.getDimensionDivision(worldContainerID);
+        }
+
         if (dim != customDimensionId) {
             double currentDimDiv = dim == -1 ? 8.0 : 1.0;
             String dimPart = worldContainerID.substring(worldContainerID.lastIndexOf(47) + 1);

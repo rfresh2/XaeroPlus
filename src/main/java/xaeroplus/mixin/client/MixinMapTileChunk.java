cff6731724e5fa6349e6b02a8b0cc2e523ddd287
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-02 17:39:23 -0700

    fix dimension switch shadow overlay

--- src/main/java/xaeroplus/mixin/client/MixinMapTileChunk.java
+++ src/main/java/xaeroplus/mixin/client/MixinMapTileChunk.java
@@ -4,12 +4,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xaero.map.MapProcessor;
 import xaero.map.cache.BlockStateShortShapeCache;
+import xaero.map.mcworld.WorldMapClientWorldData;
 import xaero.map.region.MapTile;
 import xaero.map.region.MapTileChunk;
 import xaero.map.region.texture.LeafRegionTexture;
+import xaeroplus.XaeroPlus;
 import xaeroplus.util.SeenChunksTrackingMapTileChunk;
 
 import java.io.DataInputStream;
@@ -38,6 +41,39 @@ public class MixinMapTileChunk implements SeenChunksTrackingMapTileChunk {
             for (int j = 0; j < 4; ++j) {
                 seenTiles[i][j] = leafTexture.getHeight(i << 4, j << 4) != -1;
             }
+        }
+    }
+
+    @Redirect(method = "updateBuffers", at = @At(value = "FIELD", target = "Lxaero/map/mcworld/WorldMapClientWorldData;shadowR:F"))
+    public float getShadowR(final WorldMapClientWorldData instance) {
+        if (XaeroPlus.customDimensionId == 0) {
+            return 0.518F;
+        } else if (XaeroPlus.customDimensionId == -1) {
+            return 1.0F;
+        } else {
+            return 1.0F;
+        }
+    }
+
+    @Redirect(method = "updateBuffers", at = @At(value = "FIELD", target = "Lxaero/map/mcworld/WorldMapClientWorldData;shadowG:F"))
+    public float getShadowG(final WorldMapClientWorldData instance) {
+        if (XaeroPlus.customDimensionId == 0) {
+            return 0.678F;
+        } else if (XaeroPlus.customDimensionId == -1) {
+            return .0F;
+        } else {
+            return 1.0F;
+        }
+    }
+
+    @Redirect(method = "updateBuffers", at = @At(value = "FIELD", target = "Lxaero/map/mcworld/WorldMapClientWorldData;shadowB:F"))
+    public float getShadowB(final WorldMapClientWorldData instance) {
+        if (XaeroPlus.customDimensionId == 0) {
+            return 1.0F;
+        } else if (XaeroPlus.customDimensionId == -1) {
+            return .0F;
+        } else {
+            return 1.0F;
         }
     }
 }

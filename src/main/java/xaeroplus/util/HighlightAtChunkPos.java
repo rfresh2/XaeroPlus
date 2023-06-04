54ae155277cbcd47ef70fd5ff58244bbc41d3a1d
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-06-04 15:31:49 -0700

    lighting update limiter

--- src/main/java/xaeroplus/util/HighlightAtChunkPos.java
+++ src/main/java/xaeroplus/util/HighlightAtChunkPos.java
@@ -1,5 +1,7 @@
 package xaeroplus.util;
 
+import java.util.Objects;
+
 public class HighlightAtChunkPos {
     public final int x;
     public final int z;
@@ -7,5 +9,18 @@ public class HighlightAtChunkPos {
     public HighlightAtChunkPos(int x, int z) {
         this.x = x;
         this.z = z;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final HighlightAtChunkPos that = (HighlightAtChunkPos) o;
+        return x == that.x && z == that.z;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, z);
     }
 }

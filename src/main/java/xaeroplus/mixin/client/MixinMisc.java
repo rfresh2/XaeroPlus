2b1e191327c87813eede4aa783c01cad445927cb
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-06-12 23:22:49 -0700

    disable skip world render feature

---
+++ src/main/java/xaeroplus/mixin/client/MixinMisc.java
@@ -0,0 +1,23 @@
+package xaeroplus.mixin.client;
+
+import net.minecraft.client.gui.screen.Screen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xaero.map.misc.Misc;
+import xaeroplus.settings.XaeroPlusSettingRegistry;
+
+@Mixin(value = Misc.class, remap = false)
+public class MixinMisc {
+    /**
+     * @author rfresh2
+     * @reason skipping world render messes up some events on hack clients
+     */
+    @Inject(method = "screenShouldSkipWorldRender", at = @At("HEAD"), cancellable = true)
+    private static void screenShouldSkipWorldRender(Screen screen, boolean checkOtherMod, CallbackInfoReturnable<Boolean> cir) {
+        if (!XaeroPlusSettingRegistry.skipWorldRenderSetting.getValue()) {
+            cir.setReturnValue(false);
+        }
+    }
+}

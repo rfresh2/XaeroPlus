1f79444008f9f65296139b2fd4e7c2d4767f3f1e
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-06-13 23:55:23 -0700

    allow multiple clients to open the same map

--- src/main/java/xaeroplus/mixin/client/MixinMapProcessor.java
+++ src/main/java/xaeroplus/mixin/client/MixinMapProcessor.java
@@ -17,22 +17,36 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import xaero.map.MapLimiter;
-import xaero.map.MapProcessor;
-import xaero.map.MapRunner;
-import xaero.map.WorldMap;
+import xaero.map.*;
+import xaero.map.biome.BiomeColorCalculator;
 import xaero.map.biome.BiomeGetter;
+import xaero.map.biome.BlockTintProvider;
+import xaero.map.cache.BrokenBlockTintCache;
 import xaero.map.file.MapSaveLoad;
+import xaero.map.file.worldsave.WorldDataHandler;
 import xaero.map.gui.GuiMap;
+import xaero.map.mods.SupportMods;
 import xaero.map.region.LeveledRegion;
+import xaero.map.region.MapRegion;
+import xaero.map.world.MapDimension;
 import xaero.map.world.MapWorld;
 import xaeroplus.XaeroPlus;
 import xaeroplus.util.DataFolderResolveUtil;
 import xaeroplus.util.Shared;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+
+import static xaeroplus.util.Shared.LOCK_ID;
 
 @Mixin(value = MapProcessor.class, remap = false)
 public abstract class MixinMapProcessor {
@@ -52,6 +66,45 @@ public abstract class MixinMapProcessor {
     @Shadow public Registry<Biome> worldBiomeRegistry;
     @Final
     @Shadow private BiomeGetter biomeGetter;
+    @Final
+    @Shadow public Object uiSync;
+    @Shadow private boolean mapWorldUsableRequest;
+    @Shadow private FileLock currentMapLock;
+    @Shadow private FileLock mapLockToRelease;
+    @Shadow private FileChannel mapLockChannelToClose;
+    @Shadow private FileChannel currentMapLockChannel;
+    @Shadow private boolean currentMapNeedsDeletion;
+    @Shadow private String currentWorldId;
+    @Shadow private String currentDimId;
+    @Shadow private String currentMWId;
+    @Shadow private ArrayList<Double[]> footprints;
+    @Shadow private MapWriter mapWriter;
+    @Shadow private ClientWorld newWorld;
+    @Shadow private RegistryWrapper<Block> newWorldBlockLookup;
+    @Shadow public Registry<Block> newWorldBlockRegistry;
+    @Shadow private Registry<Fluid> newWorldFluidRegistry;
+    @Shadow public Registry<Biome> newWorldBiomeRegistry;
+    @Shadow private BlockTintProvider worldBlockTintProvider;
+    @Shadow private BiomeColorCalculator biomeColorCalculator;
+    @Final
+    @Shadow private BrokenBlockTintCache brokenBlockTintCache;
+    @Shadow private WorldDataHandler worldDataHandler;
+    @Shadow private boolean waitingForWorldUpdate;
+    @Shadow
+    public abstract void popWriterPause();
+    @Shadow
+    public abstract void popRenderPause(boolean rendering, boolean uploading);
+    @Shadow
+    public abstract void addToProcess(LeveledRegion<?> region);
+    @Shadow
+    public abstract boolean isCurrentMapLocked();
+    @Shadow
+    protected abstract void clearToRefresh();
+    @Shadow
+    public abstract String getDimensionName(RegistryKey<World> id);
+    @Shadow public abstract void pushWriterPause();
+    @Shadow
+    public abstract void pushRenderPause(boolean rendering, boolean uploading);
     @Shadow
     protected abstract void forceClean();
     @Shadow
@@ -136,6 +189,250 @@ public abstract class MixinMapProcessor {
 
         if (this.state == 2) {
             this.state = 3;
+        }
+    }
+
+    /**
+     * Reason: Allow multiple client instances to open the same map
+     */
+    @Inject(method = "updateWorldSynced", at = @At("HEAD"), cancellable = true)
+    synchronized void updateWorldSynced(final CallbackInfo ci) throws IOException {
+        // @Overwrite kinda weird with these synchronized methods
+        // this gets the same effect with inject
+        ci.cancel();
+        synchronized(this.uiSync) {
+            boolean changedDimension = this.mapWorldUsable != this.mapWorldUsableRequest
+                    || !this.mapWorldUsableRequest
+                    || this.mapWorld.getFutureDimension() != this.mapWorld.getCurrentDimension();
+            if (this.mapWorldUsable != this.mapWorldUsableRequest
+                    || this.mapWorldUsableRequest
+                    && (
+                    changedDimension
+                            || !this.mapWorld.getFutureDimension().getFutureMultiworldUnsynced().equals(this.mapWorld.getFutureDimension().getCurrentMultiworld())
+            )) {
+                String newMultiworldId = !this.mapWorldUsableRequest ? null : this.mapWorld.getFutureMultiworldUnsynced();
+                this.pushRenderPause(true, true);
+                this.pushWriterPause();
+                String newWorldId = !this.mapWorldUsableRequest ? null : this.mapWorld.getMainId();
+                String newMWId = !this.mapWorld.isMultiplayer() ? null : newMultiworldId;
+                boolean shouldClearAllDimensions = this.state == 1;
+                boolean shouldClearNewDimension = this.mapWorldUsableRequest
+                        && !this.mapWorld.getFutureMultiworldUnsynced().equals(this.mapWorld.getFutureDimension().getCurrentMultiworld());
+                this.mapSaveLoad.getToSave().clear();
+                if (this.currentMapLock != null) {
+                    this.mapLockToRelease = this.currentMapLock;
+                    this.mapLockChannelToClose = this.currentMapLockChannel;
+                    this.currentMapLock = null;
+                    this.currentMapLockChannel = null;
+                }
+
+                this.releaseLocksIfNeeded();
+                if (this.mapWorld.getCurrentDimensionId() != null) {
+                    MapDimension currentDim = this.mapWorld.getCurrentDimension();
+                    MapDimension reqDim = !this.mapWorldUsableRequest ? null : this.mapWorld.getFutureDimension();
+                    boolean shouldFinishCurrentDim = this.mapWorldUsable && !this.currentMapNeedsDeletion;
+                    boolean currentDimChecked = false;
+                    if (shouldFinishCurrentDim) {
+                        this.mapSaveLoad.saveAll = true;
+                    }
+
+                    if (shouldFinishCurrentDim || shouldClearNewDimension && reqDim == currentDim) {
+                        for(LeveledRegion<?> region : currentDim.getLayeredMapRegions().getUnsyncedList()) {
+                            if (shouldFinishCurrentDim) {
+                                if (region.getLevel() == 0) {
+                                    MapRegion leafRegion = (MapRegion)region;
+                                    if (!leafRegion.isMultiplayer() && !leafRegion.hasLookedForCache() && leafRegion.isOutdatedWithOtherLayers()) {
+                                        File potentialCacheFile = this.mapSaveLoad.getCacheFile(leafRegion, leafRegion.getCaveLayer(), false, false);
+                                        if (potentialCacheFile.exists()) {
+                                            leafRegion.setCacheFile(potentialCacheFile);
+                                            leafRegion.setLookedForCache(true);
+                                        }
+                                    }
+
+                                    if (leafRegion.shouldConvertCacheToOutdatedOnFinishDim() && leafRegion.getCacheFile() != null) {
+                                        leafRegion.convertCacheToOutdated(this.mapSaveLoad, "might be outdated");
+                                        if (WorldMap.settings.debug) {
+                                            WorldMap.LOGGER.info(String.format("Converting cache for region %s because it might be outdated.", leafRegion));
+                                        }
+                                    }
+                                }
+
+                                region.setReloadHasBeenRequested(false, "world/dim change");
+                                region.onCurrentDimFinish(this.mapSaveLoad, (MapProcessor) (Object) this);
+                            }
+
+                            if (shouldClearAllDimensions || shouldClearNewDimension && reqDim == currentDim) {
+                                region.onDimensionClear((MapProcessor) (Object) this);
+                            }
+                        }
+
+                        currentDimChecked = true;
+                    }
+
+                    if (reqDim != currentDim && shouldClearNewDimension) {
+                        for(LeveledRegion<?> region : reqDim.getLayeredMapRegions().getUnsyncedList()) {
+                            region.onDimensionClear((MapProcessor) (Object) this);
+                        }
+                    }
+
+                    if (shouldClearAllDimensions) {
+                        for(MapDimension dim : this.mapWorld.getDimensionsList()) {
+                            if (!currentDimChecked || dim != currentDim) {
+                                for(LeveledRegion<?> region : dim.getLayeredMapRegions().getUnsyncedList()) {
+                                    region.onDimensionClear((MapProcessor) (Object) this);
+                                }
+                            }
+                        }
+                    }
+
+                    if (this.currentMapNeedsDeletion) {
+                        this.mapWorld.getCurrentDimension().deleteMultiworldMapDataUnsynced(this.mapWorld.getCurrentDimension().getCurrentMultiworld());
+                    }
+                }
+
+                this.currentMapNeedsDeletion = false;
+                if (!shouldClearAllDimensions) {
+                    if (shouldClearNewDimension) {
+                        this.mapWorld.getFutureDimension().regionsToCache.clear();
+                        this.mapWorld.getFutureDimension().clear();
+                        if (WorldMap.settings.debug) {
+                            WorldMap.LOGGER.info("Dimension map data cleared!");
+                        }
+                    }
+                } else {
+                    if (this.mapWorld.getCurrentDimensionId() != null) {
+                        for(MapDimension dim : this.mapWorld.getDimensionsList()) {
+                            dim.clear();
+                        }
+                    }
+
+                    if (WorldMap.settings.debug) {
+                        WorldMap.LOGGER.info("All map data cleared!");
+                    }
+
+                    if (this.state == 1) {
+                        WorldMap.LOGGER.info("World map cleaned normally!");
+                        this.state = 2;
+                    }
+                }
+
+                if (WorldMap.settings.debug) {
+                    WorldMap.LOGGER.info("World changed!");
+                }
+
+                this.mapWorldUsable = this.mapWorldUsableRequest;
+                if (this.mapWorldUsableRequest) {
+                    this.mapWorld.switchToFutureUnsynced();
+                }
+
+                this.currentWorldId = newWorldId;
+                this.currentDimId = !this.mapWorldUsableRequest ? null : ((MapProcessor) (Object) this).getDimensionName(this.mapWorld.getFutureDimensionId());
+                this.currentMWId = newMWId;
+                Path mapPath = this.mapSaveLoad.getMWSubFolder(this.currentWorldId, this.currentDimId, this.currentMWId);
+                if (this.mapWorldUsable) {
+                    Files.createDirectories(mapPath);
+                    /** allow us to have multiple clients open on the same map ;) **/
+                    Path mapLockPath = Paths.get(System.getProperty("java.io.tmpdir")).resolve(LOCK_ID + ".lock");
+                    int lockAttempts = 10;
+
+                    while (lockAttempts-- > 0) {
+                        if (lockAttempts < 9) {
+                            WorldMap.LOGGER.info("Failed attempt to lock the current world map! Retrying in 50 ms... " + lockAttempts);
+
+                            try {
+                                Thread.sleep(50L);
+                            } catch (InterruptedException var17) {
+                            }
+                        }
+
+                        try {
+                            FileChannel lockChannel = FileChannel.open(mapLockPath, StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+                            this.currentMapLock = lockChannel.tryLock();
+                            if (this.currentMapLock != null) {
+                                this.currentMapLockChannel = lockChannel;
+                                break;
+                            }
+                        } catch (Exception var18) {
+                            WorldMap.LOGGER.error("suppressed exception", var18);
+                        }
+                    }
+                }
+
+                this.footprints.clear();
+                this.mapSaveLoad.clearToLoad();
+                this.mapSaveLoad.setNextToLoadByViewing((LeveledRegion<?>) null);
+                this.clearToRefresh();
+
+                for(int i = 0; i < this.toProcessLevels.length; ++i) {
+                    this.toProcessLevels[i].clear();
+                }
+
+                if (this.mapWorldUsable && !this.isCurrentMapLocked()) {
+                    for(LeveledRegion<?> region : this.mapWorld.getCurrentDimension().getLayeredMapRegions().getUnsyncedList()) {
+                        if (region.shouldBeProcessed()) {
+                            this.addToProcess(region);
+                        }
+                    }
+                }
+
+                this.mapWriter.resetPosition();
+                this.world = this.newWorld;
+                this.worldBlockLookup = this.newWorldBlockLookup;
+                this.worldBlockRegistry = this.newWorldBlockRegistry;
+                this.worldFluidRegistry = this.newWorldFluidRegistry;
+                this.worldBiomeRegistry = this.newWorldBiomeRegistry;
+                this.worldBlockTintProvider = this.world == null
+                        ? null
+                        : new BlockTintProvider(this.worldBiomeRegistry, this.biomeColorCalculator, (MapProcessor) (Object) this, this.brokenBlockTintCache);
+                if (SupportMods.framedBlocks()) {
+                    SupportMods.supportFramedBlocks.onWorldChange();
+                }
+
+                if (SupportMods.pac()) {
+                    SupportMods.xaeroPac.onMapChange(changedDimension);
+                    SupportMods.xaeroPac.resetDetection();
+                }
+
+                if (WorldMap.settings.debug) {
+                    WorldMap.LOGGER.info("World/dimension changed to: " + this.currentWorldId + " " + this.currentDimId + " " + this.currentMWId);
+                }
+
+                this.worldDataHandler.prepareSingleplayer(this.world, (MapProcessor) (Object) this);
+                if (this.worldDataHandler.getWorldDir() == null && this.currentWorldId != null && !this.mapWorld.isMultiplayer()) {
+                    this.currentWorldId = this.currentDimId = null;
+                }
+
+                boolean shouldDetect = this.mapWorldUsable && !this.mapWorld.getCurrentDimension().hasDoneRegionDetection();
+                this.mapSaveLoad.setRegionDetectionComplete(!shouldDetect);
+                this.popRenderPause(true, true);
+                this.popWriterPause();
+            } else if (this.newWorld != this.world) {
+                this.pushWriterPause();
+                this.world = this.newWorld;
+                this.worldBlockLookup = this.newWorldBlockLookup;
+                this.worldBlockRegistry = this.newWorldBlockRegistry;
+                this.worldFluidRegistry = this.newWorldFluidRegistry;
+                this.worldBiomeRegistry = this.newWorldBiomeRegistry;
+                this.worldBlockTintProvider = this.world == null
+                        ? null
+                        : new BlockTintProvider(this.worldBiomeRegistry, this.biomeColorCalculator, (MapProcessor) (Object) this, this.brokenBlockTintCache);
+                if (SupportMods.framedBlocks()) {
+                    SupportMods.supportFramedBlocks.onWorldChange();
+                }
+
+                if (SupportMods.pac()) {
+                    SupportMods.xaeroPac.resetDetection();
+                }
+
+                this.popWriterPause();
+            }
+
+            if (this.mapWorldUsable) {
+                this.mapWorld.getCurrentDimension().switchToFutureMultiworldWritableValueUnsynced();
+                this.mapWorld.switchToFutureMultiworldTypeUnsynced();
+            }
+
+            this.waitingForWorldUpdate = false;
         }
     }
 

dd330387f989a678eee9841ce9d461b356b2a4e4
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-06-29 00:46:31 -0700

    fix update GUI button not being large enough

--- src/main/java/xaeroplus/mixin/client/MixinGuiUpdateAll.java
+++ src/main/java/xaeroplus/mixin/client/MixinGuiUpdateAll.java
@@ -2,18 +2,16 @@ package xaeroplus.mixin.client;
 
 import it.unimi.dsi.fastutil.booleans.BooleanConsumer;
 import net.minecraft.client.gui.screen.ConfirmScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xaero.common.gui.MySmallButton;
 import xaero.common.patreon.GuiUpdateAll;
 
-import java.awt.*;
-import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
 
 @Mixin(value = GuiUpdateAll.class, remap = false)
 public abstract class MixinGuiUpdateAll extends ConfirmScreen {
@@ -24,17 +22,17 @@ public abstract class MixinGuiUpdateAll extends ConfirmScreen {
 
     @Inject(method = "init", at = @At("TAIL"), remap = true)
     public void initGui(CallbackInfo ci) {
-        addButton(new MySmallButton(999,
-                                    this.width / 2 - 100,
-                                    this.height / 6 + 168,
-                                    Text.of("Go to XaeroPlus Github for updates"),
-                                    (button -> {
-                                        Desktop d = Desktop.getDesktop();
-                                        try {
-                                            d.browse(new URI("https://github.com/rfresh2/XaeroPlus"));
-                                        } catch (IOException | URISyntaxException e) {
-                                            // ???
-                                        }
-                                   })));
+        this.addDrawableChild(ButtonWidget.builder(Text.of("Go to XaeroPlus Github for updates"), (button -> {
+            try {
+                 Util.getOperatingSystem().open(new URI("https://github.com/rfresh2/XaeroPlus"));
+            } catch (Exception e) {
+                 // ???
+            }
+        }))
+        .dimensions(this.width / 2 - 100,
+            this.height / 6 + 168,
+            200,
+            20)
+        .build());
     }
 }

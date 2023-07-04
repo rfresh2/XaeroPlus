4e036556dfe1d6b909f4bf5c1e1a94b0391b751c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-04 01:06:45 -0700

    fix beacon rendering at different FOV's

--- src/main/java/xaeroplus/mixin/client/mc/MixinWorldRenderer.java
+++ src/main/java/xaeroplus/mixin/client/mc/MixinWorldRenderer.java
@@ -1,7 +1,11 @@
 package xaeroplus.mixin.client.mc;
 
+import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,17 +14,11 @@ import xaero.common.XaeroMinimapSession;
 import xaero.common.minimap.waypoints.render.WaypointsIngameRenderer;
 import xaeroplus.util.CustomWaypointsIngameRenderer;
 
-@Mixin(value = GameRenderer.class)
-public class MixinGameRenderer {
-    @Inject(
-            method = "renderWorld",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/client/render/WorldRenderer;render(Lnet/minecraft/client/util/math/MatrixStack;FJZLnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/GameRenderer;Lnet/minecraft/client/render/LightmapTextureManager;Lorg/joml/Matrix4f;)V",
-                    shift = At.Shift.AFTER
-            )
-    )
-    public void renderLevel(float tickDelta, long limitTime, MatrixStack matrixStack, CallbackInfo info) {
+@Mixin(value = WorldRenderer.class)
+public class MixinWorldRenderer {
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;checkEmpty(Lnet/minecraft/client/util/math/MatrixStack;)V", ordinal = 1, shift = At.Shift.AFTER))
+    public void renderBlockEntitiesInject(final MatrixStack matrixStack, final float tickDelta, final long limitTime, final boolean renderBlockOutline, final Camera camera, final GameRenderer gameRenderer, final LightmapTextureManager lightmapTextureManager, final Matrix4f positionMatrix, final CallbackInfo ci) {
         final XaeroMinimapSession minimapSession = XaeroMinimapSession.getCurrentSession();
         if (minimapSession == null) return;
         WaypointsIngameRenderer waypointsIngameRenderer = minimapSession.getModMain().getInterfaces().getMinimapInterface().getWaypointsIngameRenderer();

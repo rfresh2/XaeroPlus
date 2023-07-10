b19273436eae2fa70588e98a008726649b8efe97
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-09 22:07:50 -0700

    minor style change to settings registry

--- src/main/java/xaeroplus/util/newchunks/NewChunksLocalCache.java
+++ src/main/java/xaeroplus/util/newchunks/NewChunksLocalCache.java
@@ -30,7 +30,7 @@ public class NewChunksLocalCache extends NewChunksBaseCacheHandler implements Ne
             if (chunks.size() > maxNumber) {
                 if (lock.readLock().tryLock(1, TimeUnit.SECONDS)) {
                     // remove oldest 500 chunks
-                    final List<Long> toRemove = chunks.entrySet().stream()
+                    final List<Long> toRemove = chunks.long2LongEntrySet().stream()
                             .sorted(Map.Entry.comparingByValue())
                             .limit(500)
                             .map(Map.Entry::getKey)

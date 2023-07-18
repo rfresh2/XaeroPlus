05b13371b710c8b9092f4f55ba35c63809a9ba9f
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-18 12:27:51 -0700

    prevent crash if installed on a server

--- src/main/java/xaeroplus/XaeroPlus.java
+++ src/main/java/xaeroplus/XaeroPlus.java
@@ -1,6 +1,7 @@
 package xaeroplus;
 
 import com.collarmc.pounce.EventBus;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,11 +9,11 @@ import xaeroplus.module.ModuleManager;
 import xaeroplus.settings.XaeroPlusSettingRegistry;
 import xaeroplus.util.Shared;
 
-public class XaeroPlus implements ModInitializer {
+public class XaeroPlus implements ClientModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("XaeroPlus");
 	public static final EventBus EVENT_BUS = new EventBus(Runnable::run);
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		ModuleManager.init();
 		boolean a = Shared.settingsLoadedInit; // force static instances to init
 		XaeroPlusSettingRegistry.fastMapSetting.getValue(); // force static instances to init

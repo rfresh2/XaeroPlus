456bd12c7aa13061e90843a7f6fc1fc820afac13
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-20 00:10:49 -0700

    skip rendering waypoint beacons not in field of view

---
+++ src/main/java/xaeroplus/mixin/client/mc/AccessorWorldRenderer.java
@@ -0,0 +1,12 @@
+package xaeroplus.mixin.client.mc;
+
+import net.minecraft.client.render.Frustum;
+import net.minecraft.client.render.WorldRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(WorldRenderer.class)
+public interface AccessorWorldRenderer {
+    @Accessor(value = "frustum")
+    Frustum getFrustum();
+}

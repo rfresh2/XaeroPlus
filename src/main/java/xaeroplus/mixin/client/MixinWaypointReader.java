efa5d66aef729ee170532c65330a7d74483ff829
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-27 11:49:21 -0700

    add baritone right click options to waypoint elements

---
+++ src/main/java/xaeroplus/mixin/client/MixinWaypointReader.java
@@ -0,0 +1,45 @@
+package xaeroplus.mixin.client;
+
+import baritone.api.BaritoneAPI;
+import baritone.api.pathing.goals.GoalXZ;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xaero.map.gui.IRightClickableElement;
+import xaero.map.gui.dropdown.rightclick.RightClickOption;
+import xaero.map.mods.gui.Waypoint;
+import xaero.map.mods.gui.WaypointReader;
+import xaeroplus.util.BaritoneHelper;
+
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
+
+@Mixin(value = WaypointReader.class, remap = false)
+public class MixinWaypointReader {
+
+    @Inject(method = "getRightClickOptions(Lxaero/map/mods/gui/Waypoint;Lxaero/map/gui/IRightClickableElement;)Ljava/util/ArrayList;",
+        at = @At("RETURN"))
+    public void getRightClickOptionsReturn(final Waypoint element, final IRightClickableElement target, final CallbackInfoReturnable<ArrayList<RightClickOption>> cir) {
+        if (BaritoneHelper.isBaritonePresent()) {
+            final ArrayList<RightClickOption> options = cir.getReturnValue();
+            options.addAll(3, asList(
+                new RightClickOption(I18n.format("gui.world_map.baritone_goal_here"), options.size(), target) {
+                    @Override
+                    public void onAction(GuiScreen screen) {
+                        BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoal(new GoalXZ(element.getX(), element.getZ()));
+                    }
+                },
+                new RightClickOption(I18n.format("gui.world_map.baritone_path_here"), options.size(), target) {
+                    @Override
+                    public void onAction(GuiScreen screen) {
+                        BaritoneAPI.getProvider().getPrimaryBaritone().getCustomGoalProcess().setGoalAndPath(new GoalXZ(element.getX(), element.getZ()));
+                    }
+                }
+            ));
+        }
+    }
+}

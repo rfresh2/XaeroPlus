42e8b4468df7b6d53fd6fdaf0b909e2d6af10118
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-27 11:49:08 -0700

    reflection micro-optimization

--- src/main/java/xaeroplus/mixin/client/MixinMisc.java
+++ src/main/java/xaeroplus/mixin/client/MixinMisc.java
@@ -4,9 +4,12 @@ import net.minecraft.client.gui.GuiScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xaero.map.misc.Misc;
 import xaeroplus.settings.XaeroPlusSettingRegistry;
+
+import java.lang.reflect.Method;
 
 @Mixin(value = Misc.class, remap = false)
 public class MixinMisc {
@@ -20,5 +23,16 @@ public class MixinMisc {
         if (!XaeroPlusSettingRegistry.skipWorldRenderSetting.getValue()) {
             cir.setReturnValue(false);
         }
+    }
+
+    @Redirect(method = "getReflectMethodValue", at = @At(value = "INVOKE", target = "Ljava/lang/reflect/Method;setAccessible(Z)V", ordinal = 0))
+    private static void setAccessible(final Method instance, final boolean b) {
+        if (instance.isAccessible()) return; // don't set accessible if it already is lol
+        instance.setAccessible(true);
+    }
+
+    @Redirect(method = "getReflectMethodValue", at = @At(value = "INVOKE", target = "Ljava/lang/reflect/Method;setAccessible(Z)V", ordinal = 1))
+    private static void resetAccessible(final Method instance, final boolean b) {
+        // don't reset accessible lol
     }
 }

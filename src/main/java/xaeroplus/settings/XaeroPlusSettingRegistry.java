55b1f12e7a9828fb36e55445a051d7862a067e04
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-07-28 00:08:39 -0700

    update fast mapping defaults and tooltips

--- src/main/java/xaeroplus/settings/XaeroPlusSettingRegistry.java
+++ src/main/java/xaeroplus/settings/XaeroPlusSettingRegistry.java
@@ -27,7 +27,7 @@ public final class XaeroPlusSettingRegistry {
             "setting.world_map.fast_mapping_delay",
             10, 1000, 10,
             "setting.world_map.fast_mapping_delay.tooltip",
-            100,
+            90,
             SettingLocation.WORLD_MAP_MAIN);
     public static final XaeroPlusFloatSetting fastMapMaxTilesPerCycle = XaeroPlusFloatSetting.create(
             "Fast Mapping Chunk Limit",

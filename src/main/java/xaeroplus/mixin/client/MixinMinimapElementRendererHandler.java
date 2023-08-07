79adecbe41cf3da0ea9b79e52b0b7693ca266507
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-08-06 22:52:44 -0700

    correctly scale radar on WorldMap when Minimap scaling > 1

---
+++ src/main/java/xaeroplus/mixin/client/MixinMinimapElementRendererHandler.java
@@ -0,0 +1,62 @@
+package xaeroplus.mixin.client;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gl.Framebuffer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import xaero.common.AXaeroMinimap;
+import xaero.common.graphics.renderer.multitexture.MultiTextureRenderTypeRendererProvider;
+import xaero.common.minimap.element.render.MinimapElementReader;
+import xaero.common.minimap.element.render.MinimapElementRenderProvider;
+import xaero.common.minimap.element.render.MinimapElementRenderer;
+import xaero.common.minimap.element.render.MinimapElementRendererHandler;
+import xaero.common.minimap.render.MinimapRendererHelper;
+import xaero.common.minimap.render.radar.element.RadarRenderContext;
+import xaeroplus.util.IScreenRadarRenderContext;
+
+@Mixin(value = MinimapElementRendererHandler.class, remap = false)
+public class MixinMinimapElementRendererHandler {
+
+    @Inject(
+        method = "renderForRenderer",
+        at = @At(
+            value = "INVOKE",
+            target = "Lxaero/common/minimap/element/render/MinimapElementRenderer;preRender(ILnet/minecraft/entity/Entity;Lnet/minecraft/entity/player/PlayerEntity;DDDLxaero/common/AXaeroMinimap;Lnet/minecraft/client/render/VertexConsumerProvider$Immediate;Lxaero/common/graphics/renderer/multitexture/MultiTextureRenderTypeRendererProvider;)V"
+        ), locals = LocalCapture.CAPTURE_FAILHARD, remap = true)
+    public void renderForRendererInject(MinimapElementRenderer renderer,
+                                        DrawContext guiGraphics,
+                                        Entity renderEntity,
+                                        PlayerEntity player,
+                                        double renderX,
+                                        double renderY,
+                                        double renderZ,
+                                        double ps,
+                                        double pc,
+                                        double zoom,
+                                        boolean cave,
+                                        float partialTicks,
+                                        int elementIndex,
+                                        Framebuffer framebuffer,
+                                        AXaeroMinimap modMain,
+                                        MinimapRendererHelper helper,
+                                        VertexConsumerProvider.Immediate renderTypeBuffers,
+                                        TextRenderer font,
+                                        MultiTextureRenderTypeRendererProvider multiTextureRenderTypeRenderers,
+                                        int indexLimit,
+                                        CallbackInfoReturnable<Integer> cir,
+                                        MinimapElementReader elementReader,
+                                        MinimapElementRenderProvider provider,
+                                        Object context,
+                                        int location) {
+        if (context instanceof RadarRenderContext) {
+            ((IScreenRadarRenderContext) context).setIsWorldMap(false);
+        }
+    }
+}
---
+++ src/main/java/xaeroplus/mixin/client/MixinRadarRenderContext.java
@@ -0,0 +1,21 @@
+package xaeroplus.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import xaero.common.minimap.render.radar.element.RadarRenderContext;
+import xaeroplus.util.IScreenRadarRenderContext;
+
+@Mixin(value = RadarRenderContext.class, remap = false)
+public class MixinRadarRenderContext implements IScreenRadarRenderContext {
+    @Unique private boolean isWorldMap = false;
+
+    @Override
+    public boolean isWorldMap() {
+        return isWorldMap;
+    }
+
+    @Override
+    public void setIsWorldMap(final boolean isWorldMap) {
+        this.isWorldMap = isWorldMap;
+    }
+}

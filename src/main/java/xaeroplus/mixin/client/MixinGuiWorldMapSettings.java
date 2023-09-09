e9c315a5464d86f69f2dd46cb15382663319ebbe
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-09-09 14:07:47 -0700

    New XaeroPlus WorldMap settings page GUI

--- src/main/java/xaeroplus/mixin/client/MixinGuiWorldMapSettings.java
+++ src/main/java/xaeroplus/mixin/client/MixinGuiWorldMapSettings.java
@@ -6,11 +6,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xaero.map.gui.ConfigSettingEntry;
 import xaero.map.gui.GuiSettings;
 import xaero.map.gui.GuiWorldMapSettings;
 import xaero.map.gui.ISettingEntry;
-import xaeroplus.settings.XaeroPlusSettingsReflectionHax;
+import xaeroplus.util.GuiXaeroPlusWorldMapSettings;
 
 @Mixin(value = GuiWorldMapSettings.class, remap = false)
 public abstract class MixinGuiWorldMapSettings extends GuiSettings {
@@ -20,14 +19,12 @@ public abstract class MixinGuiWorldMapSettings extends GuiSettings {
 
     @Inject(method = "<init>(Lnet/minecraft/client/gui/screen/Screen;Lnet/minecraft/client/gui/screen/Screen;)V", at = @At("RETURN"), remap = true)
     public void init(final Screen parent, final Screen escapeScreen, final CallbackInfo ci) {
-        final ConfigSettingEntry[] configSettingEntries = XaeroPlusSettingsReflectionHax.getWorldMapConfigSettingEntries()
-                .toArray(new ConfigSettingEntry[0]);
         final int oldLen = this.entries.length;
-        final int newLen = configSettingEntries.length;
-        final int totalNewLen = oldLen + configSettingEntries.length;
+        final int newLen = 1;
+        final int totalNewLen = oldLen + 1;
         final ISettingEntry[] newEntries = new ISettingEntry[totalNewLen];
+        newEntries[0] = GuiXaeroPlusWorldMapSettings.getScreenSwitchSettingEntry(this);
         System.arraycopy(this.entries, 0, newEntries, newLen, oldLen);
-        System.arraycopy(configSettingEntries, 0, newEntries, 0, newLen);
         this.entries = newEntries;
     }
 }

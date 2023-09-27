903f1d187b8c95555e92c4c9a5cd55ba047471dd
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-09-26 19:58:35 -0700

    render WorldMap behind XaeroPlusWorldMapSettings

--- src/main/java/xaeroplus/util/GuiXaeroPlusWorldMapSettings.java
+++ src/main/java/xaeroplus/util/GuiXaeroPlusWorldMapSettings.java
@@ -1,5 +1,6 @@
 package xaeroplus.util;
 
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 import xaero.map.gui.GuiSettings;
@@ -14,10 +15,16 @@ public class GuiXaeroPlusWorldMapSettings extends GuiSettings {
         this.entries = XaeroPlusSettingsReflectionHax.getWorldMapConfigSettingEntries().toArray(new ISettingEntry[0]);
     }
 
+    @Override
+    public void render(DrawContext guiGraphics, int par1, int par2, float par3) {
+        this.renderEscapeScreen(guiGraphics, par1, par2, par3);
+        super.render(guiGraphics, par1, par2, par3);
+    }
+
     public static ScreenSwitchSettingEntry getScreenSwitchSettingEntry(Screen parent) {
         return new ScreenSwitchSettingEntry(
             "gui.xaeroplus.world_map_settings",
-            (current, escape) -> new GuiXaeroPlusWorldMapSettings(parent, escape),
+            GuiXaeroPlusWorldMapSettings::new,
             null,
             true
         );

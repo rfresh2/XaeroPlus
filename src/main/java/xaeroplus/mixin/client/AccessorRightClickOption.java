5e8aa79db7c36deca777d20f09a18d6182550983
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-09-28 18:04:18 -0700

    Disable Waypoint Sharing setting

---
+++ src/main/java/xaeroplus/mixin/client/AccessorRightClickOption.java
@@ -0,0 +1,12 @@
+package xaeroplus.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import xaero.map.gui.dropdown.rightclick.RightClickOption;
+
+@Mixin(value = RightClickOption.class, remap = false)
+public interface AccessorRightClickOption {
+
+    @Accessor
+    String getName();
+}

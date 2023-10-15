f1f29eb75bb185e5b8a64d37b4bb7f42f7eabae6
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-15 13:54:57 -0700

    WM1.35.0 MM23.8.3

--- src/main/java/xaeroplus/mixin/client/MixinForgeEventHandler.java
+++ src/main/java/xaeroplus/mixin/client/MixinForgeEventHandler.java
@@ -4,7 +4,6 @@ import com.mojang.authlib.GameProfile;
 import net.minecraft.network.message.MessageType;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -29,22 +28,6 @@ public abstract class MixinForgeEventHandler {
         if (XaeroPlusSettingRegistry.disableReceivingWaypoints.getValue()) {
             // cancelling at head so we avoid hitting the logic to parse the waypoint string
             cir.setReturnValue(false); // false will show the raw message in chat to the player
-            return;
-        }
-        final String textString = component.getString();
-        if (!textString.contains("xaero_waypoint:") && !textString.contains("xaero-waypoint:"))
-            return; // not a xaero waypoint message
-        if (XaeroPlusSettingRegistry.waypointReceiveNoChatReportsFix.getValue()) {
-            // check if this looks like a chat message
-            if (textString.startsWith("<") && textString.contains(">")) {
-                // get the sender name
-                final String senderName = textString.substring(textString.indexOf("<") + 1, textString.indexOf(">"));
-                // handle chat message normally but with the sender name
-                cir.setReturnValue(handleChatMessage(senderName, component));
-            }
         }
     }
-
-    @Shadow
-    protected abstract boolean handleChatMessage(String playerName, Text text);
 }

5391777ded0efd63a3bf5d9750651740659ed1e4
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-11-01 21:38:46 -0700

    migrate highlights to records

--- src/main/java/xaeroplus/util/highlights/HighlightAtChunkPos.java
+++ src/main/java/xaeroplus/util/highlights/HighlightAtChunkPos.java
@@ -1,26 +1,3 @@
 package xaeroplus.util.highlights;
 
-import java.util.Objects;
-
-public class HighlightAtChunkPos {
-    public final int x;
-    public final int z;
-
-    public HighlightAtChunkPos(int x, int z) {
-        this.x = x;
-        this.z = z;
-    }
-
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        final HighlightAtChunkPos that = (HighlightAtChunkPos) o;
-        return x == that.x && z == that.z;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, z);
-    }
-}
+public record HighlightAtChunkPos(int x, int z) { }

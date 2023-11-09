dce1754d32e7d72251cf895976e22cdfe6fc76da
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-11-09 11:56:41 -0800

    improve keybind lookup performance

--- src/main/java/xaeroplus/mixin/client/MixinControlsHandler.java
+++ src/main/java/xaeroplus/mixin/client/MixinControlsHandler.java
@@ -6,12 +6,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xaero.common.controls.ControlsHandler;
-import xaeroplus.settings.XaeroPlusBooleanSetting;
-
-import java.util.stream.Stream;
-
-import static java.util.Objects.nonNull;
-import static xaeroplus.settings.XaeroPlusSettingsReflectionHax.*;
+import xaeroplus.settings.XaeroPlusSettingsReflectionHax;
 
 @Mixin(value = ControlsHandler.class, remap = false)
 public class MixinControlsHandler {
@@ -19,16 +14,9 @@ public class MixinControlsHandler {
     @Inject(method = "keyDown", at = @At("TAIL"))
     public void keyDown(KeyBinding kb, boolean tickEnd, boolean isRepeat, CallbackInfo ci) {
         if (!tickEnd) {
-            Stream.of(XAERO_PLUS_WORLDMAP_SETTINGS.stream(),
-                            XAERO_PLUS_MINIMAP_OVERLAY_SETTINGS.stream(),
-                            XAERO_PLUS_MINIMAP_SETTINGS.stream(),
-                            XAERO_PLUS_KEYBIND_SETTINGS.stream())
-                    .flatMap(x -> x)
-                    .filter(xaeroPlusSetting -> xaeroPlusSetting instanceof XaeroPlusBooleanSetting)
-                    .map(xaeroPlusSetting -> (XaeroPlusBooleanSetting) xaeroPlusSetting)
-                    .filter(s -> nonNull(s.getKeyBinding()))
-                    .filter(s -> kb == s.getKeyBinding())
-                    .forEach(s -> s.setValue(!s.getValue()));
+            XaeroPlusSettingsReflectionHax.getKeybinds().stream()
+                    .filter(k -> k == kb)
+                    .forEach(k -> k.setPressed(true));
         }
     }
 }

44b9afc158e3072e6f7a823298045079441e30e2
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-11-29 13:08:57 -0800

    remove unused mixin

--- src/main/java/xaeroplus/mixin/client/MixinGuiCaveModeOptions.java
+++
@@ -1,9 +0,0 @@
-package xaeroplus.mixin.client;
-
-import org.spongepowered.asm.mixin.Mixin;
-import xaero.map.gui.GuiCaveModeOptions;
-
-@Mixin(value = GuiCaveModeOptions.class, remap = false)
-public class MixinGuiCaveModeOptions {
-
-}

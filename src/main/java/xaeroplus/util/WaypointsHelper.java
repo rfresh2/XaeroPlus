44ba13e246e4ac2aa8538d8cb373239943fdde6e
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-12-05 19:59:55 -0800

    WM1.37.2 MM23.9.2

--- src/main/java/xaeroplus/util/WaypointsHelper.java
+++ src/main/java/xaeroplus/util/WaypointsHelper.java
@@ -1,36 +1,6 @@
 package xaeroplus.util;
 
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
-
 public class WaypointsHelper {
-    // nullable
-    public static @Nullable RegistryKey<World> getDimensionKeyForWaypointWorldKey(String waypointWorldKey) {
-        String dimIdPart = waypointWorldKey.substring(waypointWorldKey.lastIndexOf(47) + 1).substring(4);
-        if (dimIdPart.equals("0")) {
-            return World.OVERWORLD;
-        } else if (dimIdPart.equals("1")) {
-            return World.END;
-        } else if (dimIdPart.equals("-1")) {
-            return World.NETHER;
-        } else {
-            String[] idArgs = dimIdPart.split("\\$");
-            if (idArgs.length == 1) {
-                return null;
-            } else {
-                try {
-                    Integer.parseInt(idArgs[1]);
-                    return null;
-                } catch (NumberFormatException var5) {
-                    return RegistryKey.of(RegistryKeys.WORLD, new Identifier(idArgs[0], idArgs[1].replace('%', '/')));
-                }
-            }
-        }
-    }
-
     public static int getDimensionForWaypointWorldKey(final String waypointWorldKey) {
         String dimIdPart = waypointWorldKey.substring(waypointWorldKey.lastIndexOf(47) + 1).substring(4);
         if (!dimIdPart.matches("-{0,1}[0-9]+")) {

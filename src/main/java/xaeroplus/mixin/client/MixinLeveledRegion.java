0731ff26a0a50db2491503f905f24af6316b9d3e
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-12-17 02:43:04 -0800

    meteor compat

--- src/main/java/xaeroplus/mixin/client/MixinLeveledRegion.java
+++ src/main/java/xaeroplus/mixin/client/MixinLeveledRegion.java
@@ -1,13 +1,12 @@
 package xaeroplus.mixin.client;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.Share;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xaero.map.region.LeveledRegion;
 import xaero.map.region.texture.RegionTexture;
@@ -19,12 +18,12 @@ import java.util.zip.ZipOutputStream;
 public abstract class MixinLeveledRegion<T extends RegionTexture<T>> {
     // todo: benchmark loadTexture zipfast vs current
 
-    @WrapOperation(method = "saveCacheTextures", at = @At(
+    @Redirect(method = "saveCacheTextures", at = @At(
         value = "NEW",
         args = "class=java/io/DataOutputStream"
     ))
-    public DataOutputStream replaceSaveCacheTexturesZipOutputStream(final OutputStream out, final Operation<DataOutputStream> original,
-                                                   @Share("byteOut") final LocalRef<ByteArrayOutputStream> byteOutRef) {
+    public DataOutputStream replaceSaveCacheTexturesZipOutputStream(final OutputStream out,
+                                                                    @Share("byteOut") final LocalRef<ByteArrayOutputStream> byteOutRef) {
         ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
         byteOutRef.set(byteOut);
         return new DataOutputStream(byteOut);

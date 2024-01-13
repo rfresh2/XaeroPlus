b18dc8841687890a580c5dd3b2a4be0e3e3cea7d
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-01-13 11:59:25 -0800

    also check waystones class

--- src/main/java/xaeroplus/util/WaystonesHelper.java
+++ src/main/java/xaeroplus/util/WaystonesHelper.java
@@ -1,5 +1,6 @@
 package xaeroplus.util;
 
+import net.blay09.mods.waystones.Waystones;
 import net.blay09.mods.waystones.api.event.WaystonesListReceivedEvent;
 import xaeroplus.XaeroPlus;
 
@@ -11,6 +12,7 @@ public class WaystonesHelper {
         if (!checked) {
             try {
                 // events changed in recent versions of Waystones
+                Class.forName(Waystones.class.getName());
                 Class.forName(WaystonesListReceivedEvent.class.getName());
                 XaeroPlus.LOGGER.info("Found Waystones. Enabling Waystones support.");
                 isWaystonesPresent = true;

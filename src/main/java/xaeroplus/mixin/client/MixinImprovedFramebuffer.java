bba2b2d1816fb0e54284e808a07c74accda6722d
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-02-06 22:33:52 -0800

    Minimap FPS Limiter Module

---
+++ src/main/java/xaeroplus/mixin/client/MixinImprovedFramebuffer.java
@@ -0,0 +1,33 @@
+package xaeroplus.mixin.client;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.mojang.blaze3d.systems.RenderSystem;
+import org.lwjgl.opengl.GL30;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import xaero.common.graphics.ImprovedFramebuffer;
+import xaeroplus.settings.XaeroPlusSettingRegistry;
+
+@Mixin(value = ImprovedFramebuffer.class)
+public class MixinImprovedFramebuffer {
+
+    @WrapOperation(method = "resize", at = @At(
+        value = "INVOKE",
+        target = "Lxaero/common/graphics/ImprovedFramebuffer;detectOptifineFBOs()V"
+    ))
+    public void wrapDetectOptifineFBOs(final Operation<Void> original) {
+        if (!XaeroPlusSettingRegistry.minimapFpsLimiter.getValue()) original.call();
+    }
+
+    @WrapOperation(method = "beginWrite", at = @At(
+        value = "INVOKE",
+        target = "Lcom/mojang/blaze3d/platform/GlStateManager;_glBindFramebuffer(II)V"
+    ), remap = false)
+    private static void redirectBindFramebuffer(final int i, final int j, final Operation<Void> original) {
+        if (!XaeroPlusSettingRegistry.minimapFpsLimiter.getValue()) original.call(i, j);
+        // sidestep our redirect mixin in GlStateManager to allow xaero to bind and render to its custom FBOs
+        RenderSystem.assertOnRenderThreadOrInit();
+        GL30.glBindFramebuffer(i, j);
+    }
+}

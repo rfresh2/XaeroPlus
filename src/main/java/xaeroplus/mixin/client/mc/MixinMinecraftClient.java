498edd04f5e922a01d4d4dfeefdeec735aa52eea
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-02-06 22:33:52 -0800

    Minimap FPS Limiter Module

--- src/main/java/xaeroplus/mixin/client/mc/MixinMinecraftClient.java
+++ src/main/java/xaeroplus/mixin/client/mc/MixinMinecraftClient.java
@@ -1,12 +1,16 @@
 package xaeroplus.mixin.client.mc;
 
+import com.mojang.blaze3d.pipeline.RenderTarget;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import xaeroplus.XaeroPlus;
 import xaeroplus.event.ClientTickEvent;
+import xaeroplus.module.impl.FpsLimiter;
+import xaeroplus.settings.XaeroPlusSettingRegistry;
 
 @Mixin(value = Minecraft.class)
 public class MixinMinecraftClient {
@@ -18,5 +22,12 @@ public class MixinMinecraftClient {
     @Inject(method = "tick", at = @At("RETURN"))
     public void tickReturn(final CallbackInfo ci) {
         XaeroPlus.EVENT_BUS.call(ClientTickEvent.Post.INSTANCE);
+    }
+
+    @Inject(method = "getMainRenderTarget", at = @At("HEAD"), cancellable = true)
+    public void getMainRenderTarget(CallbackInfoReturnable<RenderTarget> ci) {
+        if (!XaeroPlusSettingRegistry.minimapFpsLimiter.getValue()) return;
+        var renderTargetOverwrite = FpsLimiter.renderTargetOverwrite;
+        if (renderTargetOverwrite != null) ci.setReturnValue(renderTargetOverwrite);
     }
 }

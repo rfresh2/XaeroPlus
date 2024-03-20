752e8a2f36fdc6c317217254c34960940db9baae
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-03-19 23:41:35 -0700

    relocate sqlite to avoid DH forge classloader conflict

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -42,7 +42,7 @@ dependencyResolutionManagement {
 			library("balm-forge", "maven.modrinth:balm:7.1.4+forge-1.20.1")
 			library("fabric-waystones", "maven.modrinth:fwaystones:3.1.3+mc1.20")
 			library("worldtools", "maven.modrinth:worldtools:1.2.0+1.20.1")
-			library("sqlite", "org.xerial:sqlite-jdbc:3.45.1.0")
+			library("sqlite", "com.github.rfresh2:sqlite-jdbc:2ba0c66439") // relocated xerial sqlite to avoid conflicts with other mods
 			library("immediatelyfast", "maven.modrinth:immediatelyfast:1.2.10+1.20.4-fabric")
 			library("modmenu", "maven.modrinth:modmenu:7.2.2")
 			library("sodium", "maven.modrinth:sodium:mc1.20.1-0.5.3")

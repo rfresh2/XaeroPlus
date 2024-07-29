b4611b64ed60860820a535320dfacf5551cd51b8
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-07-29 14:20:25 -0700

    workaround ARM JVM being broken again

--- common/src/main/java/xaeroplus/feature/extensions/CustomMapProcessor.java
+++ common/src/main/java/xaeroplus/feature/extensions/CustomMapProcessor.java
@@ -2,4 +2,5 @@ package xaeroplus.feature.extensions;
 
 public interface CustomMapProcessor {
     ThreadLocal<Boolean> xaeroPlus$getLeafRegionActualDimSignal();
+    ThreadLocal<Boolean> xaeroPlus$getCurrentDimensionActualDimSignal();
 }

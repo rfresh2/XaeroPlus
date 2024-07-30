75d9e9d3d0d4207b6b5386e96ad8e8acbe2c7d07
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-07-30 00:12:16 -0700

    update to latest stable neo

--- neo/src/main/java/xaeroplus/neo/XaeroPlusConfigScreenFactory.java
+++ neo/src/main/java/xaeroplus/neo/XaeroPlusConfigScreenFactory.java
@@ -1,14 +1,15 @@
 package xaeroplus.neo;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import xaero.map.gui.GuiWorldMapSettings;
 import xaeroplus.feature.extensions.GuiXaeroPlusWorldMapSettings;
 
 public class XaeroPlusConfigScreenFactory implements IConfigScreenFactory {
+
     @Override
-    public Screen createScreen(final Minecraft mc, final Screen screen) {
+    public Screen createScreen(final ModContainer modContainer, final Screen screen) {
         return new GuiXaeroPlusWorldMapSettings(new GuiWorldMapSettings(screen), screen);
     }
 }

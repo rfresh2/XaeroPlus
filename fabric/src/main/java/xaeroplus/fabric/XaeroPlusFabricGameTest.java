873904dad696e484fa29c0dd9c587e2f6a082a4b
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-09-01 14:43:31 -0700

    todo

--- fabric/src/main/java/xaeroplus/fabric/XaeroPlusFabricGameTest.java
+++ fabric/src/main/java/xaeroplus/fabric/XaeroPlusFabricGameTest.java
@@ -7,6 +7,11 @@ import net.minecraft.gametest.framework.GameTestHelper;
 public class XaeroPlusFabricGameTest implements FabricGameTest {
     @GameTest(template = EMPTY_STRUCTURE)
     public void waitForJitCompiler(GameTestHelper context) {
+        // todo: we need to actually load more chunks and ensure xaero's writes chunk data to file
+        //      as is, the hmc test just stands in place in an empty structure
+        //      most xaero methods aren't actually getting jit compiled no matter how long we wait here
+        //      also there's probably some JVM args that could be used to force c2 jit to compile earlier
+
         context.startSequence()
             .thenIdle(20 * 60 * 10) // 10 minutes
             .thenSucceed();

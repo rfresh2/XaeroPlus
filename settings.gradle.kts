3685e1e935f1bc5826ce167ce262e0cd255ad808
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-09-06 14:56:05 -0700

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 	}
 }
 gradle.extra.apply {
-	set("mod_version", "2.23.1")
+	set("mod_version", "2.23.2")
 	set("maven_group", "xaeroplus")
 	set("archives_base_name", "XaeroPlus")
 	set("minecraft_version", "1.20.1")

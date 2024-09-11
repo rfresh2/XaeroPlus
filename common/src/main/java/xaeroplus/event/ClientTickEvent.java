86c4c3915c4b8205223717bfb341cfbadf38f69b
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-09-10 20:52:35 -0700

    minor opts

--- common/src/main/java/xaeroplus/event/ClientTickEvent.java
+++ common/src/main/java/xaeroplus/event/ClientTickEvent.java
@@ -2,10 +2,10 @@ package xaeroplus.event;
 
 public class ClientTickEvent {
     public static class Pre extends ClientTickEvent {
-        public static Pre INSTANCE = new Pre();
+        public static final Pre INSTANCE = new Pre();
     }
 
     public static class Post extends ClientTickEvent {
-        public static Post INSTANCE = new Post();
+        public static final Post INSTANCE = new Post();
     }
 }

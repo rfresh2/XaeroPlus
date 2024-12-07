71c7a3b38daae237223b8b5f049070c1105ccfe8
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2024-12-06 17:58:39 -0800

    bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 	}
 }
 gradle.extra.apply {
-	set("mod_version", "2.24.6")
+	set("mod_version", "2.24.7")
 	set("minecraft_version", "1.20.1")
 	set("parchment_version", "2023.09.03")
 	set("worldmap_version_fabric", "1.39.0")

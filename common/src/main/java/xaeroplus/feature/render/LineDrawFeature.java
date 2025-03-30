bc5ffc3e7514c62c51f99cdfa7db59a3528ca517
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-03-30 02:29:31 -0700

    simplify line preprocess function

--- common/src/main/java/xaeroplus/feature/render/LineDrawFeature.java
+++ common/src/main/java/xaeroplus/feature/render/LineDrawFeature.java
@@ -70,72 +70,54 @@ public class LineDrawFeature {
 
     public static final int MAX_LINE_LENGTH = 500_000;
 
+    // ensure no line is longer than MAX_LINE_LENGTH
+    // if a line is longer than MAX_LINE_LENGTH, split it into multiple lines each with a maximum length of MAX_LINE_LENGTH
+    // else return an empty list indicating no change is needed
+    // this is needed as otherwise we start hitting floating point precision issues as the line is rendered
     private List<Line> ensureLength(Line line) {
-        // ensure no line is longer than MAX_LINE_LENGTH
-        // if a line is longer than MAX_LINE_LENGTH, split it into multiple lines each with a maximum length of MAX_LINE_LENGTH
-        // else return an empty list indicating no change is needed
         double len = line.length();
         if (len <= MAX_LINE_LENGTH) {
             return Collections.emptyList();
         }
-        List<Line> lines = new ArrayList<>((int) (len / MAX_LINE_LENGTH + 1));
+        List<Line> lines = new ArrayList<>((int) (len / MAX_LINE_LENGTH) + 1);
         int dx = line.x2() - line.x1();
-        if (dx == 0) { // vertical line
-            int z1 = Math.min(line.z1(), line.z2());
-            int z2 = Math.max(line.z1(), line.z2());
-            int z = z1;
-            while (z < z2) {
-                int zNext = Math.min(z + MAX_LINE_LENGTH, z2);
-                lines.add(new Line(line.x1(), z, line.x2(), zNext));
-                z = zNext;
-            }
-            return lines;
-        }
         int dz = line.z2() - line.z1();
-        if (dz == 0) { // horizontal line
-            int x1 = Math.min(line.x1(), line.x2());
-            int x2 = Math.max(line.x1(), line.x2());
-            int x = x1;
-            while (x < x2) {
-                int xNext = Math.min(x + MAX_LINE_LENGTH, x2);
-                lines.add(new Line(x, line.z1(), xNext, line.z2()));
-                x = xNext;
+        if (dx == 0) { // vertical line
+            for (int z = Math.min(line.z1(), line.z2()); z < Math.max(line.z1(), line.z2()); z += MAX_LINE_LENGTH) {
+                lines.add(new Line(line.x1(), z, line.x2(), Math.min(z + MAX_LINE_LENGTH, line.z2())));
             }
-            return lines;
-        }
-        double slope = (double) dz / dx;
-        double intercept = line.z1() - slope * line.x1();
-//        int minX = Math.min(line.x1(), line.x2());
-//        int maxX = Math.max(line.x1(), line.x2());
-//        double x = minX;
-//        double z = line.z1();
-        // if positive slope, we are increasing z faster than x
-        // and therefore need to increment z by 500k
-        boolean positiveSlope = Math.abs(slope) > 1;
-        if (positiveSlope) {
-            double z1 = Math.min(line.z1(), line.z2());
-            double z2 = Math.max(line.z1(), line.z2());
-            double z = z1;
-            while (z < z2) {
-                double prevX = (z - intercept) / slope;
-                double prevZ = z;
-                z += MAX_LINE_LENGTH;
-                double x = (z - intercept) / slope;
-                lines.add(new Line((int) Math.round(prevX), (int) Math.round(prevZ), (int) Math.round(x), (int) Math.round(z)));
+        } else if (dz == 0) { // horizontal line
+            for (int x = Math.min(line.x1(), line.x2()); x < Math.max(line.x1(), line.x2()); x += MAX_LINE_LENGTH) {
+                lines.add(new Line(x, line.z1(), Math.min(x + MAX_LINE_LENGTH, line.x2()), line.z2()));
             }
         } else {
-            double x1 = Math.min(line.x1(), line.x2());
-            double x2 = Math.max(line.x1(), line.x2());
-            double x = x1;
-            while (x < x2) {
-                double prevX = x;
-                double prevZ = slope * x + intercept;
-                x += MAX_LINE_LENGTH;
-                double z = slope * x + intercept;
-                lines.add(new Line((int) Math.round(prevX), (int) Math.round(prevZ), (int) Math.round(x), (int) Math.round(z)));
+            double slope = (double) dz / dx;
+            double intercept = line.z1() - slope * line.x1();
+            // if positive slope, we are increasing z faster than x
+            // and therefore need to increment z by 500k
+            boolean positiveSlope = Math.abs(slope) > 1;
+            if (positiveSlope) {
+                for (double z = Math.min(line.z1(), line.z2()); z < Math.max(line.z1(), line.z2()); z += MAX_LINE_LENGTH) {
+                    double x = (z - intercept) / slope;
+                    lines.add(new Line(
+                        (int) Math.round((z - MAX_LINE_LENGTH - intercept) / slope),
+                        (int) Math.round(z - MAX_LINE_LENGTH),
+                        (int) Math.round(x),
+                        (int) Math.round(z)
+                    ));
+                }
+            } else {
+                for (double x = Math.min(line.x1(), line.x2()); x < Math.max(line.x1(), line.x2()); x += MAX_LINE_LENGTH) {
+                    double z = slope * x + intercept;
+                    lines.add(new Line(
+                        (int) Math.round(x - MAX_LINE_LENGTH),
+                        (int) Math.round(slope * (x - MAX_LINE_LENGTH) + intercept),
+                        (int) Math.round(x),
+                        (int) Math.round(z)
+                    ));
+                }
             }
         }
-
         return lines;
     }
 

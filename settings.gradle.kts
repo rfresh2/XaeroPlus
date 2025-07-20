3681063e292675bba22089c6a68b654896d7551c
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-07-19 22:23:25 -0700

    update baritone

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -51,9 +51,9 @@ dependencyResolutionManagement {
 //			library("fpsdisplay", "maven.modrinth:fpsdisplay:3.1.0+1.20.x")
 			library("cloth-config-fabric", "me.shedaniel.cloth:cloth-config-fabric:17.0.144")
             library("oldbiomes", "com.github.rfresh2:OldBiomes:1.0.0")
-            library("baritone-fabric", "com.github.rfresh2:baritone-fabric:1.21.7-SNAPSHOT")
-            library("baritone-forge", "com.github.rfresh2:baritone-forge:1.21.7-SNAPSHOT")
-            library("baritone-neoforge", "com.github.rfresh2:baritone-neoforge:1.21.7-SNAPSHOT")
+            library("baritone-fabric", "com.github.rfresh2:baritone-fabric:${minecraft_version}-SNAPSHOT")
+            library("baritone-forge", "com.github.rfresh2:baritone-forge:${minecraft_version}-SNAPSHOT")
+            library("baritone-neoforge", "com.github.rfresh2:baritone-neoforge:${minecraft_version}-SNAPSHOT")
         }
 	}
 }

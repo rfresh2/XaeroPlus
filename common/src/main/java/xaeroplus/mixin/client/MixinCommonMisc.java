be19315709d4bfae82a492849e692a5f3a281659
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-09-02 23:28:07 -0700

    atomic file moves if possible

--- common/src/main/java/xaeroplus/mixin/client/MixinCommonMisc.java
+++ common/src/main/java/xaeroplus/mixin/client/MixinCommonMisc.java
@@ -3,8 +3,16 @@ package xaeroplus.mixin.client;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xaero.common.misc.Misc;
+import xaeroplus.Globals;
+import xaeroplus.settings.Settings;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Mixin(value = Misc.class, remap = false)
@@ -16,6 +24,18 @@ public class MixinCommonMisc {
     ))
     private static int randomizeBackupNumber(int original) {
         // Randomize the backup number to reduce chance of concurrent backups choosing the same file name
-        return ThreadLocalRandom.current().nextInt(1, 100_000_000);
+        return ThreadLocalRandom.current().nextInt(1, 2_000_000_000);
+    }
+
+    @Inject(method = "safeMoveAndReplace", at = @At("HEAD"), cancellable = true)
+    private static void atomicMoveAndReplace(final Path from, final Path to, final boolean backupFrom, final CallbackInfo ci) throws IOException {
+        if (!Globals.atomicMoveAvailable || !Settings.REGISTRY.atomicMoveAndReplace.get()) return; // fallback to normal xaero logic
+        // skip the whole song and dance of multiple moves and deletes, just do an atomic move that replaces the target
+        try {
+            Files.move(from, to, StandardCopyOption.ATOMIC_MOVE, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception e) {
+            return; // try to fallback to normal xaero logic
+        }
+        ci.cancel();
     }
 }

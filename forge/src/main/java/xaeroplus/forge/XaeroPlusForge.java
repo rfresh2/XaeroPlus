0290e0cd9d4ebe81f6e092b0e914724cb0ab5842
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-10-30 16:16:59 -0700

    fix mixinextra loading on forge

--- forge/src/main/java/xaeroplus/forge/XaeroPlusForge.java
+++ forge/src/main/java/xaeroplus/forge/XaeroPlusForge.java
@@ -1,5 +1,6 @@
 package xaeroplus.forge;
 
+import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import net.minecraftforge.eventbus.api.bus.BusGroup;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -9,8 +10,10 @@ import net.minecraftforge.fml.loading.FMLEnvironment;
 public class XaeroPlusForge {
 
     public XaeroPlusForge(FMLJavaModLoadingContext context) {
-        BusGroup modBusGroup = context.getModBusGroup();;
+        BusGroup modBusGroup = context.getModBusGroup();
         if (FMLEnvironment.dist.isClient()) {
+            // todo: mixinextra jarjar autoload broke in 60.0.13, check back later
+            MixinExtrasBootstrap.init();
             XaeroPlusForgeClient client = new XaeroPlusForgeClient();
             client.init(context, modBusGroup);
         }

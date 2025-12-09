a31bee7906bd6d10dc644dd469853a64207bd382
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2025-12-09 15:13:34 -0800

    alternate solution for shader edit

--- common/src/main/java/xaeroplus/util/XaeroPlusGameTest.java
+++ common/src/main/java/xaeroplus/util/XaeroPlusGameTest.java
@@ -5,14 +5,6 @@ import org.spongepowered.asm.mixin.MixinEnvironment;
 public class XaeroPlusGameTest {
     public static void applyMixinsTest() {
         // forces all mixins to apply
-        try {
-            // resolve classload order issue
-            // There's a circular dependency between MapShaders and CustomRenderTypes
-            // CustomRenderTypes needs to load first, which then in its <clinit> loads MapShaders after initializing a particular static field
-            Class.forName(xaero.map.graphics.CustomRenderTypes.class.getName());
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
         MixinEnvironment.getCurrentEnvironment().audit();
     }
 }

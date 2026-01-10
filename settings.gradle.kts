3aa2f8be9bb8e942fc4164fa9ab16052792ec378
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-01-09 22:41:19 -0800

    update maven repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,10 @@
 pluginManagement {
 	repositories {
-		maven("https://maven.fabricmc.net/") { name = "Fabric" }
+		maven("https://maven.fabricmc.net/")
 		maven("https://maven.architectury.dev/")
 		maven("https://files.minecraftforge.net/maven/")
+		maven("https://maven.neoforged.net/releases")
+		maven("https://maven.2b2t.vc/remote")
 		mavenCentral()
 		gradlePluginPortal()
 	}

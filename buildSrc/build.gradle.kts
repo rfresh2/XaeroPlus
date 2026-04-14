b8d8dfc6ceeff17629012f56cc5d7cf84f92b859
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-04-13 17:09:37 -0700

    update loom

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation("gg.essential.loom:gg.essential.loom.gradle.plugin:1.15.45")
+    implementation("gg.essential.loom:gg.essential.loom.gradle.plugin:1.15.49")
     implementation("architectury-plugin:architectury-plugin.gradle.plugin:3.4-SNAPSHOT")
     implementation("com.gradleup.shadow:shadow-gradle-plugin:9.4.1")
 }

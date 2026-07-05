e685bb12117710409b342017bab87225ba75ff9a
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2026-07-05 02:27:25 -0700

    mcrt test jar

--- common/src/main/java/xaeroplus/util/XaeroPlusGameTest.java
+++ common/src/main/java/xaeroplus/util/XaeroPlusGameTest.java
@@ -6,8 +6,5 @@ public class XaeroPlusGameTest {
     public static void applyMixinsTest() {
         // forces all mixins to apply
         MixinEnvironment.getCurrentEnvironment().audit();
-        if (System.getenv("HMC_TEST") != null) {
-            Runtime.getRuntime().exit(0);
-        }
     }
 }
